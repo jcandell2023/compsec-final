9149966189a1e3197958a29075ef7d5a0942a1a1
Jeff Candell <jcandell@wustl.edu>
2021-12-19 15:41:54 -0600

    port

--- server.js
+++ server.js
@@ -307,6 +307,7 @@ io.on('connection', async (socket) => {
     })
 })
 
-server.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
     console.log('listening on http://localhost:3000')
 })
